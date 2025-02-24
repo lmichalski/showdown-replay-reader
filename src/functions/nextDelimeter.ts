6886cbbf18594b157f7827c9ed5c69fd62f4e8ad
Lucas Michalski <l.michalski93@gmail.com>
2025-02-24 16:55:00 -0500

    Update nextDelimeter.ts

--- src/functions/nextDelimeter.ts
+++ src/functions/nextDelimeter.ts
@@ -1,11 +1,11 @@
-type NextDelimeter = (s: string) => "|" | "\n";
+type NextDelimeter = (s: string) => "|" | "\n" | "";
 
 export const nextDelimeter: NextDelimeter = (s) => {
   const pipePos = s.indexOf("|");
   const newlinePos = s.indexOf("\n");
 
   if (pipePos == -1 && newlinePos == -1) {
-    throw new Error("No Delimeter found: " + s);
+    return "";
   }
 
   if (pipePos == -1) {
